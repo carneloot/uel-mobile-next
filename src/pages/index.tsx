625f35a05ea9116442abe3c9058fe255d461ff43
Matheus Carnelutt <matheuschafrao@gmail.com>
2021-06-02 01:33:11 -0300

    refactor: using router to go to map page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,22 @@
 import Head from 'next/head'
 import { FunctionComponent } from 'react'
 
-import { Data } from '@interfaces/categoria.interface'
+import { Data, Local } from '@interfaces/categoria.interface'
 import Footer from '@components/Footer'
 import Header from '@components/Header'
 
 import _data from '@content/data.json'
 
 import styles from '@styles/Home.module.scss'
+import { useRouter } from 'next/router';
 
 const Home: FunctionComponent = () => {
+    const router = useRouter();
     const data = _data as Data;
+
+    const gotoLocal = (local: Local) => {
+        return router.replace(`/mapa/${local.id}`);
+    }
 
     return (
         <div>
@@ -31,7 +37,7 @@ const Home: FunctionComponent = () => {
                         <div key={cat.id} className={styles['cat-unit']}>
                             <h2>{ cat.titulo }</h2>
                             {cat.locais.map(local =>
-                                <a key={local.id} className={styles['local-unit']} href={`/mapa/${local.id}`}>
+                                <a key={local.id} className={styles['local-unit']} onClick={() => gotoLocal(local)}>
                                     <span>{local.titulo.toUpperCase()}</span>
                                 </a>
                             )}
